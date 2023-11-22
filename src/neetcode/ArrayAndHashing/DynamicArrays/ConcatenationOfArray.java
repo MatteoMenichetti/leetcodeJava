b882d1113b4e9b5844a44947f2eee7e64f241ee4
matteo <matteo@matteo>
2023-11-22 15:39:21 +0100

    soluzione corretta

--- src/neetcode/ArrayAndHashing/DynamicArrays/ConcatenationOfArray.java
+++ src/neetcode/ArrayAndHashing/DynamicArrays/ConcatenationOfArray.java
@@ -2,10 +2,18 @@ package neetcode.ArrayAndHashing.DynamicArrays;
 
 public class ConcatenationOfArray {
 
-	
-	
+	public int[] getConcatenation(int[] nums) {
+		int[] ans = new int[nums.length * 2];
+
+		for (int i = 0; i < nums.length; i++) {
+			ans[i] = nums[i];
+			ans[nums.length + i] = nums[i];
+		}
+
+		return ans;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
